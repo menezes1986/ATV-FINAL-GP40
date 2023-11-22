3e7ddb27b17b0fedd12570879e222b4290e2119a
Miguel Angelo <migueltiojhoe@hotmail.com>
2023-11-22 11:46:15 -0300

    metodo implementado multiplicar

--- Calculadora.java
+++ Calculadora.java
@@ -1,28 +1,36 @@
 public class Calculadora {
 
-    //Método para somar os dois números:
-    
-       public int somar(int a, int b) {
-           return a + b;
-       }
-    
-    //Método para subtrair os dois números:
-    
-       public int subtrair(int a, int b) {
-           return a - b;
-       }
-    
-       public static void main(String[] args) {
-    
-    //Exemplo de uso da classe:
-    
-           Calculadora calculadora = new Calculadora();
-    
-           int resultadoSoma = calculadora.somar(5, 3);
-           System.out.println("Resultado da soma: " + resultadoSoma);
-    
-           int resultadoSubtracao = calculadora.subtrair(10, 4);
-           System.out.println("Resultado da subtração: " + resultadoSubtracao);
+    // Método para somar os dois números:
 
-       }
+    public int somar(int a, int b) {
+        return a + b;
     }
+
+    // Método para subtrair os dois números:
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    // Método para multiplicar dois números
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+
+        // Exemplo de uso da classe:
+
+        Calculadora calculadora = new Calculadora();
+
+        int resultadoSoma = calculadora.somar(5, 3);
+        System.out.println("Resultado da soma: " + resultadoSoma);
+
+        int resultadoSubtracao = calculadora.subtrair(10, 4);
+        System.out.println("Resultado da subtração: " + resultadoSubtracao);
+
+        int resultadoMultiplicacao = calculadora.multiplicar(2, 6);
+        System.out.println("Resultado da multiplicação: " + resultadoMultiplicacao);
+
+    }
+}
